353fb4a6fea035225ad5ac3593bc207cdc0d380f
adejoroemmanuel83-glitch <adejoroemmanuel83@gmail.com>
2025-09-27 13:03:48 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Mindlogic AI Tech Consulting</h1>
+      <p>Your future-ready website is now live 🚀</p>
+    </div>
+  );
+}
